ea712befc40727c84ba81291b10e1457d9821f96
andreibudoi <andreibudoi.abc@gmail.com>
2020-12-11 14:49:11 +0200

    Changed useEffect to useMemo

--- src/components/HistogramContainer.js
+++ src/components/HistogramContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useMemo } from "react";
 import Histogram from "./Histogram";
 import { ParentSize } from "@visx/responsive";
 import moment from "moment";
@@ -24,43 +24,39 @@ const sortByMonth = (array) => {
     return array;
 };
 
-const HistogramContainer = ({ loading, error, data }) => {
-    const [dataSet, setDataSet] = useState([]);
-    useEffect(() => {
-        //If fetching is done, map all the number of posts in 2019 to each month
-        if (!loading) {
-            const count = {};
-            data.allPosts.forEach((post) => {
-                if (
-                    post.published &&
-                    moment.unix(post.createdAt / 1000).format("YYYY") === "2019"
-                ) {
-                    const month = moment
-                        .unix(post.createdAt / 1000)
-                        .format("MMM");
-                    count[month] = (count[month] || 0) + 1;
-                }
-            });
+const mapPostsToMonths = (arr) => {
+    console.log("hatz");
+    if (!arr) return {};
 
-            setDataSet(
-                sortByMonth(
-                    Object.keys(count).map((key, index) => ({
-                        month: key,
-                        posts: count[key],
-                    }))
-                )
-            );
+    const count = {};
+    arr.allPosts.forEach((post) => {
+        if (
+            post.published &&
+            moment.unix(post.createdAt / 1000).format("YYYY") === "2019"
+        ) {
+            const month = moment.unix(post.createdAt / 1000).format("MMM");
+            count[month] = (count[month] || 0) + 1;
         }
-    }, []);
+    });
+
+    return sortByMonth(
+        Object.keys(count).map((key, index) => ({
+            month: key,
+            posts: count[key],
+        }))
+    );
+};
+
+const HistogramContainer = ({ loading, error, data }) => {
+    const postsPerMonth = useMemo(() => mapPostsToMonths(data), [data]); 
 
     if (loading) return "Loading histogram... ";
     if (error) return `Error fetching data! ${error.message}`;
-
     return (
         <div className="mb-3 h-100" style={{ height: "400px" }}>
             <ParentSize>
                 {({ width }) => (
-                    <Histogram dataSet={dataSet} width={width} height={250} />
+                    <Histogram dataSet={postsPerMonth} width={width} height={250} />
                 )}
             </ParentSize>
         </div>
